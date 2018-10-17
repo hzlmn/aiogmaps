944809730ab59b12f473f281f780b994a9ce7f94
Oleh Kuchuk <kuchuklehjs@gmail.com>
2018-10-17 12:44:03 +0300

    Add more places tests

--- tests/test_places.py
+++ tests/test_places.py
@@ -4,6 +4,60 @@ import aresponses
 from yarl import URL
 
 
+async def test_place(aresponses, client, api_key):
+    place_id = 'placeid'
+
+    patched_url = URL.build(
+        path='/maps/api/place/details/json',
+        query={
+            'key': api_key,
+            'placeid': place_id,
+        },
+    )
+
+    aresponses.add(
+        'maps.googleapis.com',
+        patched_url.human_repr(),
+        'get',
+        aresponses.Response(
+            body='{"status": "OK"}',
+            status=200,
+            content_type='application/json'
+        ),
+        match_querystring=True,
+    )
+
+    resp = await client.place(place_id)
+    assert resp
+
+
+async def test_places(aresponses, client, api_key):
+    patched_url = URL.build(
+        path='/maps/api/place/textsearch/json',
+        query={
+            'key': api_key,
+            'minprice': 0,
+            'maxprice': 4,
+            'query': 'Google',
+        },
+    )
+
+    aresponses.add(
+        'maps.googleapis.com',
+        patched_url.human_repr(),
+        'get',
+        aresponses.Response(
+            body='{"status": "OK"}',
+            status=200,
+            content_type='application/json'
+        ),
+        match_querystring=True
+    )
+
+    resp = await client.places('Google')
+    assert resp
+
+
 async def test_autocomplete(aresponses, client, api_key):
     patched_url = URL.build(
         path='/maps/api/place/autocomplete/json',
